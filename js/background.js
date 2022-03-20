adb7ce919f259c1828fa3289ba1a51860331422a
zxc534 <72022985+zxc534@users.noreply.github.com>
2022-03-20 23:22:08 +0900

    finish css work

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
-const images = ["0.jpg", "1.jpeg", "2.jpeg"];
+const images = ["forest.jpg", "mountains.png", "sky.png"];
 
 const randImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.src = `img/${randImage}`;
 bgImage.style.width = `${window.innerWidth}px`;
 bgImage.style.height = `${window.innerHeight}px`;
-//document.body.appendChild(bgImage);
+document.body.appendChild(bgImage);
--- js/greeting.js
+++ js/greeting.js
@@ -1,30 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username"
+const USERNAME_KEY = "username";
 
-function handleLogInSubmit(event){
+function handleLogInSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY,username);
+  localStorage.setItem(USERNAME_KEY, username);
   paintGreeting(username);
 }
 
-function paintGreeting(username){
+function paintGreeting(username) {
   greeting.innerText = `Hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if (savedUsername === null){
+if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
 } else {
   paintGreeting(savedUsername);
 }
 
-loginForm.addEventListener("submit",handleLogInSubmit);
+loginForm.addEventListener("submit", handleLogInSubmit);
